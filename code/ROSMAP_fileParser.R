a67fc40e0ef055cd725fc942ddfd462b0b756426
Gaston Sandstig <gasto@sens2023522-bianca.uppmax.uu.se>
2023-04-18 17:08:56 +0200

    started debugging parser code

--- code/ROSMAP_fileParser.R
+++ code/ROSMAP_fileParser.R
@@ -15,8 +15,12 @@ library("DESeq2")
 working_directory <- getwd()
 setwd(directory_path)
 
-count_matrix <- as.matrix(read.csv("ROSMAP_all_counts_matrix.txt"))
-rnaseq_metadata <- read.csv("ROSMAP_assay_RNAseq_metadata.csv")
+count_matrix <- as.matrix(read.csv(
+  "ROSMAP_all_counts_matrix.txt",
+  sep = "\t",
+  row.names = "feature"
+  ))
+rnaseq_metadata <- read.csv("ROSMAP_assay_rnaSeq_metadata.csv")
 clinical_metadata <- read.csv("ROSMAP_clinical.csv")
 biospecimen_metadata <- read.csv("ROSMAP_biospecimen_metadata.csv")
 
@@ -25,32 +29,63 @@ setwd(working_directory)
 # Modify to align row and column names
 
 ## Format in file is 'X<specimenID>', we substitute 'X' to ''
-colnames(count_matrix) <- sub("X", "", colnames(count_matrix))
+dimnames(count_matrix)[[2]] <- sub("X", "", dimnames(count_matrix)[[2]])
 
-## Clinical metadata has no specimenID, we check corresponding
-## specimenID from biospecimen metadata
-idx <- match(
-    clinical_metadata$individualID,
-    biospecimen_metadata$individualID
-    )
-clinical_metadata$specimenID <- biospecimen_metadata$specimenID[idx]
+## Function to remove na columns and rows
+cleanDF <- function(df) {
+  
+  # Remove na cols
+  good_cols = c()
+  for (col_idx in 1:length(df)) {
+    if (any(complete.cases(df[[col_idx]])))
+      good_cols <- append(good_cols, col_idx)
+  }
+  df <- df[,good_cols]
+  
+  # Remove na rows
+  good_rows = c()
+  for (row_idx in 1:nrow(df)) {
+    if (any(complete.cases(df[row_idx,])))
+      good_rows <- append(good_rows, row_idx)
+  }
+  df <- df[good_rows,]
+  
+  # Remove cols with no info
+  good_cols = c()
+  for (col_idx in 1:length(df)) {
+    if (!(length(unique(df[[col_idx]])) == 1))
+      good_cols <- append(good_cols, col_idx)
+  }
+  df <- df[,good_cols]
+  
+  return(df)
+}
+
 
 ## We can pick out rows of interest and discard the rest,
 ## additionally we sort them to the same order
 id_of_interest <- colnames(count_matrix)
 
-tmp <- match(id_of_interest, rnaseq_metadata$specimenID)
-rnaseq_metadata <- rnaseq_metadata[, colnames(count_matrix)]
+idx <- match(id_of_interest, rnaseq_metadata$specimenID)
+rnaseq_metadata <- rnaseq_metadata[idx,]
+rnaseq_metadata = cleanDF(rnaseq_metadata)
 
-tmp <- match(id_of_interest, clinical_metadata$specimenID)
-clinical_metadata <- clinical_metadata[, colnames(count_matrix)]
+idx <- match(id_of_interest, biospecimen_metadata$specimenID)
+biospecimen_metadata <- biospecimen_metadata[idx,]
+biospecimen_metadata = cleanDF(biospecimen_metadata)
 
-tmp <- match(id_of_interest, biospecimen_metadata$specimenID)
-biospecimen_metadata <- biospecimen_metadata[, colnames(count_matrix)]
+### Clinical metadata has no specimenID
+individualID_of_interest <- biospecimen_metadata$individualID
+idx <- match(individualID_of_interest, clinical_metadata$individualID)
+clinical_metadata <- clinical_metadata[idx,]
 
 # Merge them into one dataframe by specimenID
-annotation_df <- merge(rnaseq_metadata, clinical_metadata, by = "specimenID")
-annotation_df <- merge(annotation_df, biospecimen_metadata, by = "specimenID")
+annotation_df <- merge(rnaseq_metadata, biospecimen_metadata, by = "specimenID")
+annotation_df <- merge(annotation_df, clinical_metadata, by = "individualID")
+
+#
+# Add na rows to counts with no annotation
+#
 
 # Construct the DESeq data set (dds), no design in this function
 dds <- DESeqDataSetFromMatrix(
