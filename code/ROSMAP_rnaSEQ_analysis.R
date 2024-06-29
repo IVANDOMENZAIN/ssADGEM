2b0751a31c5debbb9c63360cd68defaf68c7a273
Iván Domenzain <ivand@chalmers.se>
2024-06-29 23:33:00 +0200

    chore: update code

--- code/ROSMAP_rnaSEQ_analysis.R
+++ code/ROSMAP_rnaSEQ_analysis.R
@@ -7,9 +7,8 @@ if (!requireNamespace("magrittr", quietly = TRUE)){
   install.packages("magrittr")}
 if (!requireNamespace("DESeq2", quietly = TRUE)){
   install.packages("DESeq2")}
-if (!require("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
+if (!require("DESeq2", quietly = TRUE))
+  BiocManager::install("DESeq2")
 # Changing working directory ===================================================
 # Setting the working directory to the directory containing this script
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
@@ -46,3 +45,15 @@ if (all(count_matrix[, "150_120419"] == count_matrix[, "150_120419_0_merged"])) 
   count_matrix <- count_matrix[, -idx]
 }
 
+annotation_df %<>%
+  filter(specimenID %in% colnames(count_matrix)) %>%
+  filter(assay == "rnaSeq") %>%
+  filter(!is.na(cogdx)) %>%
+  filter(!is.na(braaksc)) %>%
+  filter(!is.na(ceradsc)) %>%
+  # RINcontinous is missing for ~ 100 entries, but RIN is only missing one 
+  mutate(RINcontinuous = coalesce(RINcontinuous, RIN)) %>%
+  filter(!is.na(RINcontinuous)) %>%
+  filter(!is.na(pmi)) %>%
+  filter(!is.na(age_death)) #%>%
+  #filter((exclude == FALSE) | is.na(exclude))
