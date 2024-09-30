e4704d70752e08b23dd1838ed8fce6d08ab0f819
Iván Domenzain <ivand@chalmers.se>
2024-09-30 10:41:07 +0200

    fix: bugs in ROSMAP data processing

--- code/preprocess_data.R
+++ code/preprocess_data.R
@@ -34,14 +34,14 @@ annotation    <- read_delim(file = 'data/ROSMAP_annotation_samples.txt',delim = 
 counts_matrix <- as.data.frame(counts_matrix)
 rownames(counts_matrix) <- t(gene_ids)
 # analyze distributions per batch
-#let's see the batches at disposal
+#let's see the batches
 batches <- as.factor(annotation$Batch)
 #
 #Samples belonging to multiple batches were found, let's delete them from the study
 idx2rmv       <- which(annotation$Batch == "0, 6, 7")
 annotation    <- annotation[-idx2rmv,]
 counts_matrix <- counts_matrix[,-idx2rmv]
-#delete thos entries with 0 counts across all samples
+#delete thos gene entries with 0 counts across all samples
 idx2rmv       <- which(rowSums(counts_matrix)==0)
 counts_matrix <- counts_matrix[-idx2rmv,]
 df_counts     <- as.data.frame(t(counts_matrix))
@@ -69,26 +69,6 @@ df_counts$AD <- annotation$AD
 # file_name <- paste(resultsPath,"/ROSMAP_counts_perBatch.pdf",sep="")
 # ggsave(file_name, p, width = 12, height = 10, units = "cm",dpi = 400)
 # 
-# p <- ggplot(counts2plot, aes(x=batch, y=cpm)) +
-#   geom_boxplot() + theme_minimal() + scale_fill_wa_d(wacolors$volcano)#+ ylim(c(0,100))   #+
-# file_name <- paste(resultsPath,"/ROSMAP_counts_boxplot.pdf",sep="")
-# ggsave(file_name, p, width = 12, height = 10, units = "cm",dpi = 400)
-#Distributions of gene counts for batch 0 look a bit different to the rest, let's test 
-#using Kolmogorov-Smirnov test
-# test_matrix <- matrix(1, 9, 9) 
-# for (i in 0:8){
-#   dist_i <- counts2plot$cpm[counts2plot$batch==i]
-#   for (j in 0:8){
-#     dist_j   <- counts2plot$cpm[counts2plot$batch==j]
-#     #if(j<i){
-#     KSresult <- ks.test(dist_i,dist_j, alternative = "two.sided")
-#     test_matrix[i+1,j+1] <- KSresult$p.value
-#     #}
-#   }
-# }
-norm_matrix  <-counts_matrix/colSums(counts_matrix)
-meanSEQdepth <- mean(colSums(counts_matrix))
-norm_matrix  <- norm_matrix*meanSEQdepth
 t_c_matrix   <- t(counts_matrix)
 t_c_matrix <- as.data.frame(t(counts_matrix))
 t_c_matrix$batch <- annotation$Batch
@@ -108,7 +88,7 @@ t_c_matrix      <- as.data.frame(t_c_matrix)
 t_c_matrix$batch <- batch_col
 #pca_object      <- prcomp(t_c_matrix, center = TRUE, scale. = TRUE)
 #autoplot(pca_object, data= t_c_matrix, colour = 'batch')
-cpm_matrix_f <- counts_matrix_f/(colSums(counts_matrix_f)/1E6)
+#cpm_matrix_f <- counts_matrix_f/(colSums(counts_matrix_f)/1E6)
 sample_ID <- AD_col
 sample_bt <- batch_col
 toRemove <- which(AD_col=='other')
@@ -118,7 +98,7 @@ sample_bt <- sample_bt[-toRemove]
 
 counts_matrix.processed <- counts_matrix_f[,-toRemove]
 sample_ID <- paste(sample_ID,1:length(sample_ID),sep='_')
-sample_ID[which(grepl('No_AD_',sample_ID))] <- 'No_AD'
+sample_ID[which(grepl('No_AD_',sample_ID))] <- 'NCI'
 sample_ID <- as.data.frame(sample_ID)
 orderPos <- order(sample_ID$sample_ID)
 annotation <- annotation[orderPos,]
@@ -138,8 +118,8 @@ t_c_matrix       <- t(counts_matrix.processed)
 pca_object       <- prcomp(t_c_matrix, center = TRUE, scale. = TRUE)
 t_c_matrix       <- as.data.frame(t_c_matrix)
 t_c_matrix$batch <- as.factor(sample_bt)
-#autoplot(pca_object, data= t_c_matrix, colour = 'batch')
-#remove two oulier samples (PC2) from batch 5
+autoplot(pca_object, data= t_c_matrix, colour = 'batch')
+#remove two oulier samples (PC2) from batch 5. (145_120419)
 x    <- pca_object$x
 pos  <- which(x[,1]==max(x[,1]))
 pos2 <- which(x[,2]==min(x[,2]))
@@ -153,8 +133,8 @@ t_c_matrix       <- t(counts_matrix.processed)
 pca_object       <- prcomp(as.matrix(t_c_matrix), center = TRUE, scale. = TRUE)
 t_c_matrix       <- as.data.frame(t_c_matrix)
 t_c_matrix$batch <- as.factor(sample_bt)
-#autoplot(pca_object, data= t_c_matrix, colour = 'batch')
-#remove last outlier
+autoplot(pca_object, data= t_c_matrix, colour = 'batch')
+#remove last outlier. 122_120418
 x <- pca_object$x
 pos2 <- which(x[,2]==min(x[,2]))
 counts_matrix.processed <- counts_matrix.processed[,-pos2]
@@ -164,17 +144,19 @@ sample_bt <- sample_bt[-pos2]
 idx2rmv         <- which(rowSums(counts_matrix.processed)==0)
 counts_matrix.processed <- counts_matrix.processed[-idx2rmv,]
 t_c_matrix     <- t(counts_matrix.processed)
+pca_object      <- prcomp(as.matrix(t_c_matrix[]), center = TRUE, scale. = TRUE)
 t_c_matrix       <- as.data.frame(t_c_matrix)
 t_c_matrix$batch <- as.factor(sample_bt)
-#pca_object      <- prcomp(as.matrix(t_c_matrix), center = TRUE, scale. = TRUE)
-#autoplot(pca_object, data= t_c_matrix, colour = 'batch')
+autoplot(pca_object, data= t_c_matrix, colour = 'batch')
 #
 #remove batch effect-
-adjusted <- ComBat_seq(as.matrix(counts_matrix.processed), batch=annotation$Batch, group=NULL)
-counts.norm <- counts_matrix.processed/(colSums(adjusted)/1E6)
-#pca_object      <- prcomp(t(adjusted), center = TRUE, scale. = TRUE)
-#autoplot(pca_object, data= t_c_matrix, colour = 'batch')
-#counts_matrix.adj <- adjusted
+adjusted <- ComBat_seq(as.matrix(counts_matrix.processed), batch=annotation$Batch, group=annotation$AD)
+counts.norm <- adjusted/(colSums(adjusted)/1E6)
+
+pca_object      <- prcomp(t(adjusted), center = TRUE, scale. = TRUE)
+autoplot(pca_object, data= t_c_matrix, colour = 'batch')
+
+counts_matrix.adj <- adjusted
 #cpm_matrix.adj <-log2(counts_matrix.adj/(colSums(counts_matrix.adj)/1E6))
 
 df_counts     <- as.data.frame(t(counts.norm))
@@ -185,14 +167,14 @@ counts2plot$batch <- as.factor(counts2plot$batch)
 counts2plot$counts <- as.numeric(counts2plot$counts)
 counts2plot$AD <- as.factor(counts2plot$AD)
 counts2plot$lcpm <- log10(counts2plot$counts)
-#dds <- estimateSizeFactors(dds) dds <- estimateDispersions(dds) dds <- nbinomWaldTest(dds)
+
 p <- ggplot(counts2plot, aes(x=batch, y=lcpm)) +
   geom_boxplot() + theme_minimal() + scale_fill_wa_d(wacolors$volcano)#+ ylim(c(0,100))   #+
 file_name <- paste(resultsPath,"/ROSMAP_cpm_boxplot.pdf",sep="")
 ggsave(file_name, p, width = 12, height = 10, units = "cm",dpi = 400)
 
 #get counts per million of transcripts
-counts.norm <- counts_matrix.processed/(colSums(adjusted)/1E6)
+#counts.norm <- counts_matrix.processed/(colSums(adjusted)/1E6)
 #for diff expr analysis⁄Keep those genes with at least 1 cpm in 50% of the NCI samples
 AD_idxs <- which(annotation$AD=='AD')
 NCIidxs <- which(annotation$AD=='No_AD')
@@ -205,12 +187,26 @@ annotation$patient <- patient
 
 write.table(annotation,file = 'data/ROSMAP_annotation_processed.txt',row.names= FALSE,col.names=TRUE,sep= "\t")
 tempMat <- adjusted[toKeep,]
+tempMat <- counts_matrix.processed[toKeep,]
 write.table(tempMat,file = 'data/ROSMAP_counts_processed_4DE.txt',row.names= FALSE,col.names=TRUE,sep= "\t")
 genes <- rownames(adjusted)[toKeep]
 write.table(genes,file = 'data/ROSMAP_genes_processed.txt',row.names= FALSE,col.names=TRUE,sep= "\t")
 adjusted <- as.data.frame(adjusted)
 adjusted$genes <- rownames(adjusted)
+#get the pan-RNA sample
+mat <- as.matrix(adjusted[,1:(ncol(adjusted)-1)])
+#normalize to CPM and keep those genes with  >=1 CPM in at least one sample (permissive approach)
+norm.counts <- mat/(colSums(mat)/1E6)
+norm.counts.logical <- norm.counts>=1
+norm.counts.logical <- rowSums(norm.counts.logical)
+toKeep <- which(norm.counts.logical>=1)
+adjusted <- adjusted[toKeep,]
 write.table(adjusted,file = 'data/ROSMAP_counts_processed_all.txt',row.names= FALSE,col.names=TRUE,sep= "\t")
+newDF <- data.frame(gene = adjusted$genes,counts = rowMeans(adjusted[,1:(ncol(adjusted)-1)]))
+newDF$gene <- gsub("\\..*","",newDF$gene)
+write.table(newDF,file = 'data/ROSMAP_meanCounts_processed_all.txt',row.names= FALSE,col.names=TRUE,sep= "\t")
+
+
 #plot counts per batch after batch adjustment
 # adjusted.f <- counts.norm[toKeep,]
 # df_counts     <- as.data.frame(t(adjusted.f))
